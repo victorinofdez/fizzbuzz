1ea2d728181607bf4282d5aa01a9a62b78ca7f15
Victorino Fernández <victorino_fdez@icloud.com>
2026-01-12 13:05:51 +0100

    arreglo

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -10,25 +10,30 @@ En esta kata, se solicita al usuario un numero, si este es:
 - Si no es divisible ni entre 3 ni entre 5, debe imprimir el número
 '''
 
-def div3(numero) {
+def div3(numero):
     return (numero % 3) == 0 
-}
-def div5(numero) {
-    return (numero % 3) == 0 
-}
-def div3y5(numero) {
-    return (numero % 3) == 0 and (numero % 5) == 0
-}
 
-if __name__ == "__main__":
+def div5(numero):
+    return (numero % 5) == 0 
+
+def div3y5(numero):
+    return (numero % 3) == 0 and (numero % 5) == 0
+
+def fizzbuzz():
     inp = '' 
     while True:
-        inp = int(input())
-        if div3(inp):
-            print("Fizz\n")
-        elif div5(inp):
-            print("Buzz\n")
-        elif div3y5(inp):
-            print("FizzBuzz\n")
+        inp = int(input('Introduce un número del 1 al 100: '))
+        if inp >= 1 and inp <= 100: 
+            if div3(inp):
+                print("Fizz\n")
+            elif div5(inp):
+                print("Buzz\n")
+            elif div3y5(inp):
+                print("FizzBuzz\n")
+            else:
+                print(inp)   
         else:
-            print(inp)    
+            print("Número no valido.") 
+
+if __name__ == "__main__":
+    fizzbuzz()
