393e9d54a9c9cc5cd0c1907dd74c8b5ea2dd6454
Victorino Fernández <victorino_fdez@icloud.com>
2026-01-15 12:23:53 +0100

    def functions

--- test/test_fizzbuzz.py
+++ test/test_fizzbuzz.py
@@ -1,10 +1,48 @@
 # Divisible entre 3, el programa debe imprimir "Fizz"
+
+def test_numero_divisible_entre_3_imprime_fizz():
+    pass
+
 # Divisible entre 5, el programa debe imprimit "Buzz"
+
+def test_numero_divisible_entre_5_imprime_buzz():
+    pass
+
 # Divisible entre 3 y entre 5, el programa debe imprimir "FizzBuzz"
+
+def test_numero_divisible_entre_3_y_5_imprime_fizzbuzz():
+    pass
+
 # input de número negativo
+
+def test_input_numero_negativo_lanza_error():
+    pass
+
 # input de número cero
+
+def test_input_cero_lanza_error():
+    pass
+
 # input de numero decimal
+
+def test_input_numero_decimal_lanza_error():
+    pass
+
 # input de un carácter no numérico
+
+def test_input_caracter_no_numerico_lanza_error():
+    pass
+
 # input de una candena de texto
+
+def test_input_cadena_texto_lanza_error():
+    pass
+
 # input de número por encima de 100
+
+def test_input_numero_mayor_a_100_lanza_error():
+    pass
+
 # input de número por debajo de 1
+def test_input_numero_menor_a_1_lanza_error():
+    pass
