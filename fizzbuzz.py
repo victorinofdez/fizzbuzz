825982f8876c8d092b8293bfc737e1cc2c655722
Victorino Fernández <victorino_fdez@icloud.com>
2026-01-15 13:09:35 +0100

    fizz -> str

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,7 +9,7 @@ En esta kata, se solicita al usuario un numero, si este es:
 
 - Si no es divisible ni entre 3 ni entre 5, debe imprimir el número
 '''
-
+'''
 def fizzbuzz():
     continuar = True
 
@@ -36,8 +36,27 @@ def fizzbuzz():
 
         except Exception:
             print("Error en la entrada. Introduce un número válido.\n")
+'''
+def fizzbuzz(numero: int) -> str:
+    if type(numero) is not int:
+        raise TypeError("Debe ser un entero")
+
+    if numero < 1 or numero > 100:
+        raise ValueError("El número debe estar entre 1 y 100")
+
+    if numero % 3 == 0 and numero % 5 == 0:
+        return "FizzBuzz"
+    elif numero % 3 == 0:
+        return "Fizz"
+    elif numero % 5 == 0:
+        return "Buzz"
+    else:
+        return str(numero)
 
 
 if __name__ == "__main__":
-    fizzbuzz()
-
+    print(fizzbuzz(15))
+    print(fizzbuzz(90))
+    print(fizzbuzz(1))
+    print(fizzbuzz(3))
+    print(fizzbuzz(5))
