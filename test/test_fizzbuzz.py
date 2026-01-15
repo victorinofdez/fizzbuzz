998b52aaa9e94df291c66f0fc502af7e5edfdf90
Victorino Fernández <victorino_fdez@icloud.com>
2026-01-15 12:19:24 +0100

    tests

---
+++ test/test_fizzbuzz.py
@@ -0,0 +1,10 @@
+# Divisible entre 3, el programa debe imprimir "Fizz"
+# Divisible entre 5, el programa debe imprimit "Buzz"
+# Divisible entre 3 y entre 5, el programa debe imprimir "FizzBuzz"
+# input de número negativo
+# input de número cero
+# input de numero decimal
+# input de un carácter no numérico
+# input de una candena de texto
+# input de número por encima de 100
+# input de número por debajo de 1
